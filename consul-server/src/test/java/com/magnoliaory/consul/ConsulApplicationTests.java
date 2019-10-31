c648ea6186553ff526ed5033999865f1569c86fa
JoryB <489282384@qq.com>
2019-10-31 20:11:44 +0800

    2019.10.31

---
+++ consul-server/src/test/java/com/magnoliaory/consul/ConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnoliaory.consul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka-client/src/main/java/com/magnoliaory/eurekaclient/controller/ClientController.java
@@ -0,0 +1,22 @@
+package com.magnoliaory.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class ClientController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String di() {
+        String s = "Service" + discoveryClient.getServices();
+        System.out.println(s);
+        return s;
+    }
+}
